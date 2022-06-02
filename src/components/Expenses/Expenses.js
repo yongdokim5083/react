d452094e443b6d95c169043de811e5571bc31024
hli-2190154 <kyd5083>
2022-06-02 10:14:48 +0900

    4th upload

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,33 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpenseFilter from "./ExpensesFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
-  const items = props.items.map((data) => (
-    <ExpenseItem
-      key={data.id}
-      title={data.title}
-      amount={data.amount}
-      date={data.date}
-    />
-  ));
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  return <Card className="expenses">{items}</Card>;
+  const filterChangeHandler = (selecetedYear) => {
+    setFilteredYear(selecetedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expense={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,7 +49,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -93,6 +93,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          cancle
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
